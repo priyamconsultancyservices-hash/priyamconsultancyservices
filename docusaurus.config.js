901b941c1f9b326eeb72451d5e7d47d4a52ef8e2
soundarya <priyamconsultancyservices@gmail.com>
2026-06-18 18:01:34 +0530

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Digital Transformation and Digital Marketing Agency in India',
+  title: 'Priyam Consultancy Services | Digital Marketing, HR & Business Solutions',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/pcs-fav-icon.webp',
 
